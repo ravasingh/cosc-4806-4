de6cd4e3e4a579a85f7536a9b41ef47f600eb6b6
ravasingh <ravasingh@algomau.ca>
2024-06-28 19:49:28 +0000

    Updated user model

--- app/models/User.php
+++ app/models/User.php
@@ -10,9 +10,9 @@ class User {
 
     }
 
-    public function test () {
+    public function test() {
       $db = db_connect();
-      $statement = $db->prepare("select * from users;");
+      $statement = $db->prepare("SELECT * from users;");
       $statement->execute();
       $rows = $statement->fetch(PDO::FETCH_ASSOC);
       return $rows;
@@ -33,45 +33,31 @@ class User {
         $statement->execute();
         return $statement->fetch(PDO::FETCH_ASSOC) ? true : false;
     }
-
-
     public function authenticate($username, $password) {
 
-    $username = strtolower($username);
-    $db = db_connect();
-        $statement = $db->prepare("select * from users WHERE username = :username;");
+        $username = strtolower($username);
+        $db = db_connect();
+        $statement = $db->prepare("SELECT * from users WHERE username = :name;");
         $statement->bindValue(':name', $username);
         $statement->execute();
         $rows = $statement->fetch(PDO::FETCH_ASSOC);
 
-    if (password_verify($password, $rows['password'])) {
-      $_SESSION['auth'] = 1;
-      $_SESSION['username'] = ucwords($username);
-      unset($_SESSION['failedAuth']);
-      header('Location: /home');
-      die;
-    } else {
-      if(isset($_SESSION['failedAuth'])) {
-        $_SESSION['failedAuth'] ++; //increment
-      } else {
-        $_SESSION['failedAuth'] = 1;
-      }
-      header('Location: /login');
-      die;
+        if (password_verify($password, $rows['password'])) {
+            $_SESSION['auth'] = 1;
+            $_SESSION['username'] = ucwords($username);
+            unset($_SESSION['failedAuth']);
+            header('Location: /home');
+            die;
+        } else {
+            if(isset($_SESSION['failedAuth'])) {
+                $_SESSION['failedAuth'] ++; //increment
+            } else {
+                $_SESSION['failedAuth'] = 1;
+            }
+            header('Location: /login');
+            die;
+        }
     }
-    }
-  public function logAttempt($username, $status) {
-      $sql = "INSERT INTO login_attempts (username, status, attempt_time) VALUES (:username, :status, :attempt_time)";
-      $stmt = $this->database->prepare($sql);
-      $stmt->bindParam(':username', $username);
-      $stmt->bindParam(':status', $status);
-      $stmt->bindParam(':attempt_time', date('Y-m-d H:i:s'));
-      $stmt->execute();
-  }
-  public function getRecentAttempts($username)
-  {
-      $this->db->query('SELECT * FROM log WHERE username = :username ORDER BY attempt_time DESC LIMIT 3');
-      $this->db->bind(':username', $username);
-      return $this->db->resultSet();
-  }
+
+
 }
