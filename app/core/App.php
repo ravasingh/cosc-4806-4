684687ef47eced470b2c5a48e05190706b5a7e06
ravasingh <ravasingh@algomau.ca>
2024-06-28 19:25:17 +0000

    Renamed home.php, login.php and Making sure that home controller can be opened properly

--- app/core/App.php
+++ app/core/App.php
@@ -1,71 +1,101 @@
 <?php
 
 class App {
-
-    protected $controller = 'login';
+    protected $controller = 'Home';
     protected $method = 'index';
-    protected $special_url = ['apply'];
     protected $params = [];
 
     public function __construct() {
-        if (isset($_SESSION['auth']) == 1) {
-            //$this->method = 'index';
-            $this->controller = 'home';
-        } 
-
-        // This will return a broken up URL
-        // it will be /controller/method
         $url = $this->parseUrl();
 
-        /* if controller exists in the URL, then go to it
-         * if not, then go to this->controller which is defaulted to home 
-         */
+        // Check for specific routes first
+        $this->handleSpecificRoutes($url);
 
-        if (file_exists('app/controllers/' . $url[1] . '.php')) {
-            $this->controller = $url[1];
-
-            $_SESSION['controller'] = $this->controller;
-
-            /* This is if we have a special URL in the index.
-             * For example, our apply page is public and in the index method
-             * We do not want the method to be login in this case, but instead index
-             * 
-             */
-            if (in_array($this->controller, $this->special_url)) { 
-              $this->method = 'index';
-            }
-            unset($url[1]);
-        } else {
-            header('Location: /home');
-            die;
+        // Default controller and method if specific routes are not matched
+        if (file_exists('app/controllers/' . ucfirst($url[0]) . '.php')) {
+            $this->controller = ucfirst($url[0]);
+            unset($url[0]);
         }
 
         require_once 'app/controllers/' . $this->controller . '.php';
-
         $this->controller = new $this->controller;
 
-        // check to see if method is passed
-        // check to see if it exists
-        if (isset($url[2])) {
-            if (method_exists($this->controller, $url[2])) {
-                $this->method = $url[2];
-                $_SESSION['method'] = $this->method;
-                unset($url[2]);
+        if (isset($url[1])) {
+            if (method_exists($this->controller, $url[1])) {
+                $this->method = $url[1];
+                unset($url[1]);
             }
         }
 
-        // This will rebase the params to a new array (starting at 0)
-        // if params exist
         $this->params = $url ? array_values($url) : [];
-        call_user_func_array([$this->controller, $this->method], $this->params);		
+
+        call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
     public function parseUrl() {
-        $u = "{$_SERVER['REQUEST_URI']}";
-        //trims the trailing forward slash (rtrim), sanitizes URL, explode it by forward slash to get elements
-        $url = explode('/', filter_var(rtrim($u, '/'), FILTER_SANITIZE_URL));
-		unset($url[0]);
-		return $url;
+        if (isset($_GET['url'])) {
+            return explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+        }
     }
 
+    private function handleSpecificRoutes($url) {
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+        switch ($uri) {
+            case '/login':
+                $this->controller = 'Login';
+                $this->method = 'index';
+                break;
+            case '/login/authenticate':
+                $this->controller = 'Login';
+                $this->method = 'authenticate';
+                break;
+            case '/register':
+                $this->controller = 'register';
+                $this->method = 'index';
+                break;
+            case '/register/create':
+                $this->controller = 'register';
+                $this->method = 'create';
+                break;
+            case '/home':
+                $this->controller = 'Home';
+                $this->method = 'index';
+                break;
+            case '/notes':
+                $this->controller = 'Notes';
+                $this->method = 'index';
+                break;
+            case '/notes/create':
+                $this->controller = 'Notes';
+                $this->method = 'create';
+                break;
+            case (preg_match('/\/notes\/edit\/\d+/', $uri) ? true : false):
+                $this->controller = 'Notes';
+                $this->method = 'edit';
+                $this->params = [(int) substr($uri, strrpos($uri, '/') + 1)];
+                break;
+            case (preg_match('/\/notes\/delete\/\d+/', $uri) ? true : false):
+                $this->controller = 'Notes';
+                $this->method = 'delete';
+                $this->params = [(int) substr($uri, strrpos($uri, '/') + 1)];
+                break;
+            default:
+                $this->controller = 'Login';
+                $this->method = 'index';
+                break;
+        }
+
+        // Require and initialize the controller
+        $controllerPath = 'app/controllers/' . ucfirst($this->controller) . '.php';
+        if (file_exists($controllerPath)) {
+            require_once $controllerPath;
+            $this->controller = new $this->controller;
+        } else {
+            throw new Exception("Controller file $controllerPath not found");
+        }
+
+        // Clear $url to prevent further processing
+        $url = [];
+    }
 }
