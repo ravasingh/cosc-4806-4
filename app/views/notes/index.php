23aa1a9386b70a81fd64fb979104d3227d0849cd
ravasingh <ravasingh@algomau.ca>
2024-07-12 17:44:14 +0000

    Assignment 5: Reminders index.php page is modified

--- app/views/notes/index.php
+++ app/views/notes/index.php
@@ -1,20 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Notes</title>
-</head>
-<body>
-    <h1>Your Notes</h1>
-    <a href="/notes/create">Create Note</a>
-    <ul>
-        <?php foreach ($data['notes'] as $note): ?>
-            <li>
-                <?php echo htmlspecialchars($note['subject']); ?>
-                <a href="/notes/edit/<?php echo $note['id']; ?>">Edit</a>
-                <a href="/notes/delete/<?php echo $note['id']; ?>">Delete</a>
-            </li>
-        <?php endforeach; ?>
-    </ul>
-</body>
-</html>
+<?php require_once 'app/views/templates/header.php' ?>
+<div class="container">
+    <div class="page-header">
+        <h1>Your Reminders</h1>
+    </div>
+    <a href="/notes/create" class="btn btn-primary">Create New Reminder</a>
+    <br><br>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Subject</th>
+                <th>Created At</th>
+                <th>Completed</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($data['notes'] as $note): ?>
+                <tr>
+                    <td><?= htmlspecialchars($note['subject']) ?></td>
+                    <td><?= htmlspecialchars($note['created_at']) ?></td>
+                    <td><?= $note['completed'] ? 'Yes' : 'No' ?></td>
+                    <td>
+                        <a href="/notes/edit/<?= $note['id'] ?>" class="btn btn-warning">Edit</a>
+                        <a href="/notes/delete/<?= $note['id'] ?>" class="btn btn-danger">Delete</a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
+<?php require_once 'app/views/templates/footer.php' ?>
