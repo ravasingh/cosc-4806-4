57ef4398ba90c4466cbc5008d08ee10f18a71a94
ravasingh <ravasingh@algomau.ca>
2024-07-12 19:25:02 +0000

    Assignment 5: Updated public header template

--- app/views/templates/headerPublic.php
+++ app/views/templates/headerPublic.php
@@ -1,18 +1,25 @@
+<?php include 'header.php'; ?>
 <?php
-if (isset($_SESSION['auth']) == 1) {
+session_start(); // Ensure session is started
+if (isset($_SESSION['auth']) && $_SESSION['auth'] == 1) {
     header('Location: /home');
+    exit;
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-    <link rel="stylesheet" href="/public/css/styles.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="icon" href="/favicon.png">
-    <title>COSC 4806</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width">
-    <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="mobile-web-app-capable" content="yes">
-</head>
-<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="/login">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/create">Register</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<div class="container mt-5">
