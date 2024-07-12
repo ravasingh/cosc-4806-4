d2d8ff3a7bcc8a3453fc64aafc022259e9de5afd
ravasingh <ravasingh@algomau.ca>
2024-07-12 14:31:16 +0000

    Assignment 5: Updated home.php to get reminders information

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -3,11 +3,8 @@
 class Home extends Controller {
 
     public function index() {
-      $user = $this->model('User');
-      $data = $user->test();
-			
-	    $this->view('home/index');
-	    die;
+        $noteModel = $this->model('Note');
+        $notes = $noteModel->getAllNotes($_SESSION['user_id']);
+        $this->view('home/index', ['notes' => $notes]);
     }
-
 }
