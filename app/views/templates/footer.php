88d6ba091466aeeff55475e3e9d7441bd62edc0e
ravasingh <ravasingh@algomau.ca>
2024-07-12 18:22:21 +0000

    Assignment 5: Updated footer template

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,14 +1,8 @@
-<footer class="footer">    
-    <div class="row">
-        <div class="col-lg-12">
-            <p>Don't have an account? <a href="app/controllers/register.php">Register here</a></p>
-            <p>Copyright &copy; <?php echo date('Y'); ?> </p>
-        </div>
+<footer class="footer mt-auto py-3 bg-light">
+    <div class="container">
+        <span class="text-muted">&copy; 2024 All rights reserved.</span>
     </div>
 </footer>
-
-</div>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="public/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
